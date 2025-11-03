ea019254a7953666c588516421c7ff1087349e42
Arina Pavlova <molokova_2002@mail.ru>
2025-11-03 17:25:58 +0300

    Fix grpc_server

--- app/grpc_server.py
+++ app/grpc_server.py
@@ -1,13 +1,13 @@
 from concurrent import futures
 import grpc
-import model_service_pb2
-import model_service_pb2_grpc
-from app.models import ModelTrainer
-from app.storage import Storage
-from app.logger import log
+from . import model_service_pb2
+from . import model_service_pb2_grpc
+from .models import ModelTrainer
+from .storage import Storage
+from .logger import log
 import pickle
 
-class ModelService(model_pb2_grpc.ModelServiceServicer):
+class ModelService(model_service_pb2_grpc.ModelService):
     def __init__(self):
         self.trainer = ModelTrainer()
 
@@ -17,11 +17,11 @@ class ModelService(model_pb2_grpc.ModelServiceServicer):
         model = self.trainer.train(request.name, data.X, data.y, 
 **dict(request.params))
         Storage.save(request.name, model)
-        return model_pb2.TrainResponse(status="ok")
+        return model_service_pb2.TrainResponse(status="ok")
 
     def ListModels(self, request, context):
         log.info("List models gRPC")
-        return model_pb2.ListResponse(models=["forest", "logreg"])
+        return model_service_pb2.ListResponse(models=["forest", "logreg"])
 
     def Predict(self, request, context):
         log.info(f"Predict gRPC {request.name}")
@@ -29,14 +29,13 @@ class ModelService(model_pb2_grpc.ModelServiceServicer):
         if not model:
             context.set_details("Model not found")
             context.set_code(grpc.StatusCode.NOT_FOUND)
-            return model_pb2.PredictResponse()
+            return model_service_pb2.PredictResponse()
         pred = model.predict([request.features])[0]
-        return model_pb2.PredictResponse(pred=pred)
+        return model_service_pb2.PredictResponse(pred=pred)
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    model_pb2_grpc.add_ModelServiceServicer_to_server(ModelService(), 
-server)
+    model_service_pb2_grpc.add_ModelServiceServicer_to_server(ModelService(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     server.wait_for_termination()
