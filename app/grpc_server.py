85d0b48904efd684629d7d6a55353bef1762b302
Arina Pavlova <molokova_2002@mail.ru>
2025-11-09 13:12:34 +0300

    Updated gRPC server to use ModelServiceServicer

--- app/grpc_server.py
+++ app/grpc_server.py
@@ -7,7 +7,7 @@ from .storage import Storage
 from .logger import log
 import pickle
 
-class ModelService(model_service_pb2_grpc.ModelService):
+class ModelService(model_service_pb2_grpc.ModelServiceServicer):
     def __init__(self):
         self.trainer = ModelTrainer()
 
@@ -32,6 +32,32 @@ class ModelService(model_service_pb2_grpc.ModelService):
             return model_service_pb2.PredictResponse()
         pred = model.predict([request.features])[0]
         return model_service_pb2.PredictResponse(pred=pred)
+	
+    def RetrainModel(self, request, context):
+        """Retrain an existing model."""
+        log.info(f"RETRAIN gRPC {request.model_id}")
+        model = Storage.load(request.model_id)
+        if not model:
+            context.set_code(grpc.StatusCode.NOT_FOUND)
+            return app.model_service_pb2.TrainResponse(status="not found")
+        data = pickle.loads(request.data)
+        model_type = Storage.load_metadata().get(request.model_id, {}).get("type", "logreg")
+        new_model = self.trainer.train(model_type, data["X"], data["y"], **dict(request.params))
+        Storage.save(request.model_id, new_model, model_type, dict(request.params))
+        return app.model_service_pb2.TrainResponse(status="retrained")
+
+    def DeleteModel(self, request, context):
+        """Delete a trained model."""
+        log.info(f"DELETE gRPC {request.model_id}")
+        if Storage.delete(request.model_id):
+            return app.model_service_pb2.TrainResponse(status="deleted")
+        context.set_code(grpc.StatusCode.NOT_FOUND)
+        return app.model_service_pb2.TrainResponse(status="not found")
+
+    def Health(self, request, context):
+        """Check the health status of the service."""
+        log.info("Health check gRPC")
+        return app.model_service_pb2.TrainResponse(status="healthy")
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
